51c7284dc176194e03f84e8261c048a857ddfd8c
Rishabh Singh <140408377+rishabhx01@users.noreply.github.com>
2025-01-20 18:50:04 +0000

    slice and splice in js

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -55,9 +55,22 @@ console.log(myArray.indexOf(9));      // Output - 0   .. because 9 exist in the 
 
 const newArray = myArray.join()
 
-console.log(myArray);
-console.log(newArray);  // join changed the array type from object to string.
+console.log(myArray);                // Output - [ 9, 1, 2, 3, 4, 5, 6 ] 
+console.log(newArray);               // Output - 9,1,2,3,4,5,6              .join changed the array type from object to string.
 console.log(typeof newArray);        // Output - string
 
 // Slice, Splice
+
+console.log("A ", myArray);
+
+const myn1 = myArray.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArray);
+
+
+const myn2 = myArray.splice(1, 3)
+console.log("C ", myArray);
+console.log(myn2);
+
 
